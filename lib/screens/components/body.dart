1ab7153d2d15ede59f6a5173dd4ccb6a89253705
ArchishmanSengupta <archishman1808@gmail.com>
2021-02-02 00:11:06 +0530

    Boxshape Clock structure made

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,5 +1,8 @@
+import 'package:analog_clock/constants.dart';
 import 'package:analog_clock/size_config.dart';
 import 'package:flutter/material.dart';
+
+import 'time_in_hour_and_minute.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -11,35 +14,39 @@ class Body extends StatelessWidget {
           Text("Part Street Kolkata, India | IST",
               style: Theme.of(context).textTheme.bodyText1),
           TimeInHouseAndMinute(),
+          Clock(),
         ],
       ),
     );
   }
 }
 
-class TimeInHouseAndMinute extends StatefulWidget {
+class Clock extends StatefulWidget {
   @override
-  _TimeInHouseAndMinuteState createState() => _TimeInHouseAndMinuteState();
+  _ClockState createState() => _ClockState();
 }
 
-class _TimeInHouseAndMinuteState extends State<TimeInHouseAndMinute> {
-  TimeOfDay _timeOfDay = TimeOfDay.now();
+class _ClockState extends State<Clock> {
   @override
   Widget build(BuildContext context) {
-    String _period = _timeOfDay.period == DayPeriod.am ? "AM" : "PM";
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          "${_timeOfDay.hour}:${_timeOfDay.minute}",
-          style: Theme.of(context).textTheme.headline1,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              shape: BoxShape.circle
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0,0),
+                  color: kShadowColor.withOpacity(0.14),
+                  blurRadius: 64,
+                ),
+            ],
+          ),
         ),
-        SizedBox(width: 5),
-        Text(
-          _period,
-          style: TextStyle(fontSize: getProportionateScreenWidth(18)),
-        ),
-      ],
+      ),
     );
   }
 }
