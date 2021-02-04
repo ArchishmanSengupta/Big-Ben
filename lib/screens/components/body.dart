41d7abfb89d517825d4929146d36be8e8fe1d50d
ArchishmanSengupta <archishman1808@gmail.com>
2021-02-04 19:11:45 +0530

    hour, minute, sec hand added

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -30,23 +30,77 @@ class _ClockState extends State<Clock> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
       child: AspectRatio(
         aspectRatio: 1,
         child: Container(
           decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.surface,
-              shape: BoxShape.circle
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0,0),
-                  color: kShadowColor.withOpacity(0.14),
-                  blurRadius: 64,
-                ),
+            color: Theme.of(context).colorScheme.surface,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                offset: Offset(0, 0),
+                color: kShadowColor.withOpacity(0.14),
+                blurRadius: 64,
+              ),
             ],
+          ),
+          child: CustomPaint(
+            painter: ClockPainter(context),
           ),
         ),
       ),
     );
+  }
+}
+
+class ClockPainter extends CustomPainter {
+  final BuildContext context;
+
+  ClockPainter(this.context);
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    double centerX = size.width / 2;
+    double centerY = size.height / 2;
+    Offset center = Offset(centerX, centerY);
+
+    //hour line
+    canvas.drawLine(
+      center,
+      Offset(90, 90),
+      Paint()
+        ..color = Theme.of(context).colorScheme.secondary
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 10,
+    );
+
+    //Minutes Hand
+    canvas.drawLine(
+      center,
+      Offset(40, 180),
+      Paint()
+        ..color = Theme.of(context).accentColor
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 10,
+    );
+
+    //Second hand
+    canvas.drawLine(center, Offset(80, 150),
+        Paint()..color = Theme.of(context).primaryColor);
+
+    //This is for the middle small dot
+    Paint dotPainter = Paint()
+      ..color = Theme.of(context).primaryIconTheme.color;
+    canvas.drawCircle(center, 24, dotPainter);
+    canvas.drawCircle(
+        center, 23, Paint()..color = Theme.of(context).backgroundColor);
+    canvas.drawCircle(center, 10, dotPainter);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
