106aae605f71fbe9f8348b74500fdae911435ba0
ArchishmanSengupta <archishman1808@gmail.com>
2021-02-04 22:59:26 +0530

    slider made

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:analog_clock/size_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'clock.dart';
 import 'time_in_hour_and_minute.dart';
@@ -18,46 +19,94 @@ class Body extends StatelessWidget {
             Spacer(),
             Clock(),
             Spacer(),
-            SizedBox(
-              width: getProportionateScreenWidth(233),
-              child: AspectRatio(
-                aspectRatio: 1.32,
-                child: Container(
-                  padding: EdgeInsets.all(getProportionateScreenWidth(20)),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(
-                      color: Theme.of(context).primaryIconTheme.color,
-                    ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  CountryCard(
+                    country: "New York, USA",
+                    timeZone: "+3 HRS | EST",
+                    iconSrc: "assets/icons/Liberty.svg",
+                    time: "9:20",
+                    period: "PM",
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Kolkata, India",
-                        style: Theme.of(context).textTheme.headline4.copyWith(
-                            fontSize: getProportionateScreenWidth(16)),
-                      ),
-                      SizedBox(height: 5),
-                      Text("0 HRS | IST"),
-                      Row(
-                        children: [
-                          Text(
-                            "9 : 20",
-                            style: Theme.of(context).textTheme.headline4,
-                          ),
-                          RotatedBox(
-                            quarterTurns: 3,
-                            child: Text("AM"),
-                          ),
-                        ],
-                      )
-                    ],
+                  CountryCard(
+                    country: "New York, USA",
+                    timeZone: "+3 HRS | EST",
+                    iconSrc: "assets/icons/Liberty.svg",
+                    time: "9:20",
+                    period: "PM",
                   ),
-                ),
+                ],
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CountryCard extends StatelessWidget {
+  const CountryCard({
+    Key key,
+    @required this.country,
+    @required this.timeZone,
+    @required this.iconSrc,
+    @required this.time,
+    @required this.period,
+  }) : super(key: key);
+  final String country, timeZone, iconSrc, time, period;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(left: getProportionateScreenWidth(20)),
+      child: SizedBox(
+        width: getProportionateScreenWidth(233),
+        child: AspectRatio(
+          aspectRatio: 1.32,
+          child: Container(
+            padding: EdgeInsets.all(getProportionateScreenWidth(20)),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  country,
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4
+                      .copyWith(fontSize: getProportionateScreenWidth(16)),
+                ),
+                SizedBox(height: 5),
+                Text(timeZone),
+                Spacer(),
+                Row(
+                  children: [
+                    SvgPicture.asset(
+                      iconSrc,
+                      width: getProportionateScreenWidth(40),
+                    ),
+                    Spacer(),
+                    Text(
+                      time,
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(period),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
