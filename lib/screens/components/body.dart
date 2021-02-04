efa9b828993f5e2c565980838c5db686786b18cb
ArchishmanSengupta <archishman1808@gmail.com>
2021-02-04 23:06:00 +0530

    Succesful run. Done

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -2,6 +2,7 @@ import 'package:analog_clock/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import 'card.dart';
 import 'clock.dart';
 import 'time_in_hour_and_minute.dart';
 
@@ -31,82 +32,16 @@ class Body extends StatelessWidget {
                     period: "PM",
                   ),
                   CountryCard(
-                    country: "New York, USA",
-                    timeZone: "+3 HRS | EST",
-                    iconSrc: "assets/icons/Liberty.svg",
-                    time: "9:20",
-                    period: "PM",
+                    country: "Sydney, AUS",
+                    timeZone: "+19 HRS | EST",
+                    iconSrc: "assets/icons/Sydney.svg",
+                    time: "1:20",
+                    period: "AM",
                   ),
                 ],
               ),
             )
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class CountryCard extends StatelessWidget {
-  const CountryCard({
-    Key key,
-    @required this.country,
-    @required this.timeZone,
-    @required this.iconSrc,
-    @required this.time,
-    @required this.period,
-  }) : super(key: key);
-  final String country, timeZone, iconSrc, time, period;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: getProportionateScreenWidth(20)),
-      child: SizedBox(
-        width: getProportionateScreenWidth(233),
-        child: AspectRatio(
-          aspectRatio: 1.32,
-          child: Container(
-            padding: EdgeInsets.all(getProportionateScreenWidth(20)),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: Theme.of(context).primaryIconTheme.color,
-              ),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  country,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline4
-                      .copyWith(fontSize: getProportionateScreenWidth(16)),
-                ),
-                SizedBox(height: 5),
-                Text(timeZone),
-                Spacer(),
-                Row(
-                  children: [
-                    SvgPicture.asset(
-                      iconSrc,
-                      width: getProportionateScreenWidth(40),
-                    ),
-                    Spacer(),
-                    Text(
-                      time,
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                    RotatedBox(
-                      quarterTurns: 3,
-                      child: Text(period),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
         ),
       ),
     );
