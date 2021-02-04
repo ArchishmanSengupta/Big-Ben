7b759be8a498adf96e4b30290a8ba7beb8fc3a62
ArchishmanSengupta <archishman1808@gmail.com>
2021-02-04 21:40:39 +0530

    country card added

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:analog_clock/size_config.dart';
 import 'package:flutter/material.dart';
 
 import 'clock.dart';
@@ -6,15 +7,26 @@ import 'time_in_hour_and_minute.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      child: Column(
-        children: [
-          Text("Part Street Kolkata, India | IST",
-              style: Theme.of(context).textTheme.bodyText1),
-          TimeInHouseAndMinute(),
-          Clock(),
-        ],
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Text("Part Street Kolkata, India | IST",
+                style: Theme.of(context).textTheme.bodyText1),
+            TimeInHouseAndMinute(),
+            Spacer(),
+            Clock(),
+            Spacer(),
+            SizedBox(
+              width: getProportionateScreenWidth(233),
+              child: AspectRatio(
+                aspectRatio: 1.32,
+                child: Container(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
